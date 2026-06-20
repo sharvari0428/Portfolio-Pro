5e4a13631e37ee406035da0015a761060a2f6681
sharvari0428 <sharvaridhende06@gmail.com>
2026-06-20 19:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+window.addEventListener("scroll", function(){
 
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.style.background="#0f172a";
+}
+else{
+header.style.background="#111827";
+}
+
+});
