4699933927865dc08c863cbd52c2ec0a9618f123
sharvari0428 <sharvaridhende06@gmail.com>
2026-06-20 19:57:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,3 +72,23 @@ btn.innerHTML="🌙";
 }
 
 }
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
