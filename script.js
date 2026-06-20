18c93d18ea9fd5ab81e43a05c3a6f516f0716f36
sharvari0428 <sharvaridhende06@gmail.com>
2026-06-20 19:30:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-window.addEventListener("scroll", function(){
+const text = "Aspiring Web Developer";
 
-const header = document.querySelector("header");
+let i = 0;
 
-if(window.scrollY > 50){
-header.style.background="#0f172a";
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
 }
+
+}
+
+typing();
+
+window.addEventListener("scroll",function(){
+
+const header=document.querySelector("header");
+
+if(window.scrollY>50){
+
+header.style.background="#0f172a";
+
+}
+
 else{
+
 header.style.background="#111827";
+
 }
 
 });
